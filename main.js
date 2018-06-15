c9342b1e726a66f24eaaf94b7d3897bda7bb4264
Elijah-Walker <Elijah.Walker747@gmail.com>
2018-06-15 09:35:39 -0600

    asteroids

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function preload() {
     game.load.image('ice-platform', 'assets/Gray-UFO.png');
     game.load.image('star', 'assets/star.png');
     game.load.spritesheet('player', 'assets/UnicornFramesLarger.png', 64, 64);
-    game.load.image("asteroid", "assets/asteroid.png");
+    game.load.image("asteroid", "assets/Asteroid.png");
     game.load.audio("background-music", "assets/backgroundMusic.mp3");
     game.load.audio("collect-sound", "assets/collectSound.mp3");
     game.load.audio("jump-sound", "assets/jumpSound.mp3");
